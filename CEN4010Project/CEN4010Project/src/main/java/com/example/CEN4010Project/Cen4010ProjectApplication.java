0c9330e52411875abbf5ccccd69bfbe2b74a314b
Davidx203 <123432538+Davidx203@users.noreply.github.com>
2023-01-28 13:12:28 -0500

    Add files via upload

---
+++ CEN4010Project/CEN4010Project/src/main/java/com/example/CEN4010Project/Cen4010ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CEN4010Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cen4010ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cen4010ProjectApplication.class, args);
+	}
+
+}
---
+++ CEN4010Project/CEN4010Project/src/test/java/com/example/CEN4010Project/Cen4010ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CEN4010Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cen4010ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
