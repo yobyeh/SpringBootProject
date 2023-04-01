1d0960ac16418be0d0e415a76b3262855f32f1dc
braulio8610 <111910039+braulio8610@users.noreply.github.com>
2023-03-31 23:27:18 -0400

    Delete src/main/java/com/example/CENproject/Repository directory

--- src/main/java/com/example/CENproject/Repository/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Book.App.BookApp.Repository;
-
-import com.Book.App.BookApp.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book,Integer> {
-}
